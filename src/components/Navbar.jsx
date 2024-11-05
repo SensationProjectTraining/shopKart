f180affb9c92ae59d2305d895b8e32ba220b3250
ParveenPasricha <pasricha.parveen@yahoo.in>
2024-11-05 13:16:32 +0530

    feat: Navbar Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,10 @@ const Navbar = () => {
   const { currentUser, logout } = useUserContext();
 
   return (
-    <div className='bg-slate-400 text-2xl text-right'>
-      <Link className="px-6 hover:text-white hover:bg-blue-700" to="/">Home</Link>
+    <div className='bg-white text-2xl text-center flex'>
+      <div className='px-6'><img src="https://cms-assets.tutsplus.com/cdn-cgi/image/width=850/uploads/users/1631/posts/40055/image-upload/Screenshot_2022_02_16_at_9_30_14_am_copy.jpg"  width={100} height={100} alt="Logo" /></div>
+      <div className='py-5 w-full'>
+        <Link className="px-6 hover:text-white hover:bg-blue-700" to="/">Home</Link>
       <Link className="px-6 hover:text-white hover:bg-blue-700" to="/Shopping">Shopping</Link>
       <Link className="px-6 hover:text-white hover:bg-blue-700" to="/Cart">Cart</Link>
       {currentUser ? (
@@ -22,9 +24,11 @@ const Navbar = () => {
             Logout
           </button>
         </>
+        
       ) : (
         <Link className="px-6 hover:text-white hover:bg-blue-700" to="/login">Login/SignUp</Link>
       )}
+    </div>
     </div>
   );
 };
