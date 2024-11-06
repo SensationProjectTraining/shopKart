187a6c2c0d44efc3ad9fadc8e27c8de53eb329de
ParveenPasricha <pasricha.parveen@yahoo.in>
2024-11-07 00:37:40 +0530

    Feat: Searchbox Added

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-
+import { IoMdSearch } from "react-icons/io";
 const SearchBar = () => {
+  function SearchClick(){
+    alert("SearchBox is Active")
+  }
   return (
-    <div>
-      <input type="search" placeholder='Search your Required'></input>
-      <h1>Searchbar page open</h1>
+    <div className='flex justify-center'>
+      <input className='w-96 text-1xl border rounded-full text-black border-r-8 red px-2 h-10' type="search" placeholder='Search your Required' />
+      <h1 className="text-4xl" onClick={SearchClick}>{<IoMdSearch/>}</h1>
     </div>
   )
 }
