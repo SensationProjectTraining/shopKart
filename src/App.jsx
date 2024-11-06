a560d972214ff890a4d11aefa4b9f584cde93514
manpretS6 <manpreetsaini1122@gmail.com>
2024-11-06 12:18:50 +0530

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import Home from './components/Pages/Home'
 import Login from './components/Pages/Login'
 import { UserProvider } from './Context/Context'
 import SignUp from './components/Pages/SignUp'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <UserProvider>
       <BrowserRouter>
-        <Navbar/>
+        <Navbar/><br/>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/login" element={<Login/>}/>
           <Route path="/signup" element={<SignUp/>}/>
-        </Routes>
+        </Routes><br/>
+        <Footer/>
       </BrowserRouter>
     </UserProvider>
   )
